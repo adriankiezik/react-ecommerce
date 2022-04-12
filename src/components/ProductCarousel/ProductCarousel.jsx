0f43cc9a5f89d3f04087ec53242ccda2e2e9636b
adriankiezik <kiezikadrian@gmail.com>
2022-04-12 19:08:56 +0200

    Products aren't hardcoded anymore

--- src/components/ProductCarousel/ProductCarousel.jsx
+++ src/components/ProductCarousel/ProductCarousel.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { carouselBreakpoints } from "./CarouselConfig";
 
+import { products } from "../../data/products";
 import Product from "./Product/Product";
 
 const ProductCarousel = () => {
@@ -11,87 +13,19 @@ const ProductCarousel = () => {
       slidesPerView={1.5}
       loop={true}
       centeredSlides={true}
-      breakpoints={{
-        350: {
-          slidesPerView: 1.7,
-        },
-        400: {
-          slidesPerView: 1.9,
-        },
-        500: {
-          slidesPerView: 2.4,
-        },
-        650: {
-          slidesPerView: 3,
-        },
-        767: {
-          slidesPerView: 2.65,
-        },
-        850: {
-          slidesPerView: 3,
-        },
-        1000: {
-          slidesPerView: 3.5,
-        },
-        1200: {
-          slidesPerView: 4,
-        },
-        1350: {
-          slidesPerView: 4.5,
-        },
-        1500: {
-          slidesPerView: 5,
-        },
-        1600: {
-          slidesPerView: 5.5,
-        },
-        1800: {
-          slidesPerView: 6,
-        },
-      }}
+      breakpoints={carouselBreakpoints}
     >
-      <SwiperSlide>
-        <Product
-          image="/assets/product-1.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Product
-          image="/assets/product-2.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Product
-          image="/assets/product-3.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Product
-          image="/assets/product-4.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Product
-          image="/assets/product-5.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Product
-          image="/assets/product-6.jpg"
-          title="T-Shirt Summer Vibes"
-          price="$19.99"
-        />
-      </SwiperSlide>
+      {products.map((product) => {
+        return (
+          <SwiperSlide key={product.id}>
+            <Product
+              image={product.image}
+              title={product.name}
+              price={"$" + product.price}
+            />
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
